5def3bb9bc1dc996097ca06a22ce6eed182d2d1c
AlfanMask <afanh123123@gmail.com>
2025-01-31 08:50:26 +0700

    fix

--- src/routes/api/drivers/get/is_driver_by_user_id/+server.ts
+++ src/routes/api/drivers/get/is_driver_by_user_id/+server.ts
@@ -4,8 +4,9 @@ import { json } from '@sveltejs/kit';
 export async function GET({ url }) {
     const userId = url.searchParams.get("user-id");
 
-    const is_driver: boolean = await mysqlconn.query<any>(`SELECT id FROM drivers WHERE driver_id = '${userId}'`).then(([rows, fields]) => {
-        return rows[0].id ? true : false;
+    const is_driver: boolean = await mysqlconn.query<any>(`SELECT COUNT(id) FROM drivers WHERE driver_id = '${userId}'`).then(([rows, fields]) => {
+        if (rows[0]['COUNT(id)'] > 0) return true
+        return false;
     })
 
     return json(is_driver)
